81155bf9382aeb87406fc1f2b5f18d3b4a1c0691
Aastha010503 <aasthasingh75518@gmail.com>
2024-06-23 21:16:02 +0530

    "Added useState and useEffect hooks to Player component, introduced API call to fetch movie data"

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
 
 const Player = () => {
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at:"",
+    typeOf: ""
+  })
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMmRiMWZhMGVmMjVmNDBhN2FjMjVjZTdjZWRkNGZjYSIsIm5iZiI6MTcxOTE1NjU4MS4xMDMxNTgsInN1YiI6IjY2NzQ0MTdhYWNmMmQxYjkxMGZjNWUyYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Q9MFBx2I3_38SHFH3TH4Wy1-zqr6MvGbFDH7V0LeR4s'
+    }
+  };
+  
+  useEffect(()=>{
+    fetch('https://api.themoviedb.org/3/movie/1022789/videos?language=en-US', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+  },[])
+
   return (
     <div className='player'>
       <img src={back_arrow_icon} alt="" />
